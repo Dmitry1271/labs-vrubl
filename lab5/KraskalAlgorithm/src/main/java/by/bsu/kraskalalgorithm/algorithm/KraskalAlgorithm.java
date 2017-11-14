0b0ab30ba4307147928cdcca31dd5e07e727fc5b
Dmitry <Dmitrij1271@gmail.com>
2017-11-14 23:09:20 +0300

    Created Prim and Kraskal algorithms int the lab5

---
+++ lab5/KraskalAlgorithm/src/main/java/by/bsu/kraskalalgorithm/algorithm/KraskalAlgorithm.java
@@ -0,0 +1,86 @@
+package by.bsu.kraskalalgorithm.algorithm;
+
+import by.bsu.kraskalalgorithm.entity.Edge;
+import by.bsu.kraskalalgorithm.entity.Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by cplus on 14.11.2017.
+ */
+public class KraskalAlgorithm {
+    private int[][] matrix;
+    private int size;
+    private List<Edge> result = new ArrayList<>();
+    private Map<Integer, Integer> remarks = new HashMap<>();
+
+    public KraskalAlgorithm() {
+    }
+
+    public KraskalAlgorithm(Graph graph) {
+        matrix = graph.getMatrix();
+        size = matrix.length;
+        setRemarks();
+    }
+
+    public List<Edge> getResult() {
+        return result;
+    }
+
+    public void doAlgorithm() {
+        Edge edge;
+        //while (!isEnd()) {
+        edge = getMinAdjacentEdge();
+        changeRemark(remarks.get(edge.getV2()), remarks.get(edge.getV1()));
+        result.add(edge);
+        //}
+    }
+
+    public int getTreeWeight() {
+        int totalWeight = 0;
+        for (Edge edge : result) {
+            totalWeight += edge.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public boolean isEnd() {
+        return result.size() == size - 1;
+    }
+
+    private void setRemarks() {
+        for (int i = 0; i < size; ++i) {
+            remarks.put(i + 1, i + 1);
+        }
+    }
+
+    private Edge getMinAdjacentEdge() {
+        int min = Integer.MAX_VALUE;
+        Edge edge = new Edge();
+
+        for (int j = 0; j < size; ++j) {
+            for (int i = 0; i < size; ++i) {
+                if (matrix[j][i] > 0 && matrix[j][i] < min) {
+                    if (remarks.get(j + 1) != remarks.get(i + 1)) {
+                        min = matrix[j][i];
+                        edge = new Edge(j + 1, i + 1, matrix[j][i]);
+                    }
+                }
+            }
+        }
+        return edge;
+    }
+
+    private void changeRemark(int old, int recent) {
+        // System.out.println("fd");
+        for (int i = 0; i < size; ++i) {
+            if (remarks.get(i + 1) == old) {
+                //System.out.println("))))))))))))))))))))))))))))))))))))))))");
+                remarks.put(i + 1, recent);
+            }
+        }
+    }
+}
